1c6d38f00b8857642d4a3269cad302b4d0daf372
ryan <mail@interwebz.space>
2016-09-22 11:06:07 -0600

    organized everything into functions

--- wordsy.py
+++ wordsy.py
@@ -1,38 +1,45 @@
 import requests
 import json
 
-r = requests.get('https://openapi.etsy.com/v2/listings/trending?api_key=uyvwtl04yi98duy546afittr&offset=400&limit=50')
-#print "Response Code:", r.status_code
-#decoded = json.loads(r.text)
-decoded = json.loads(r.text)
 keywords = []
 wordlist = []
-wordfreq = []
+iterkeywords = []
 
-for item in decoded['results']:
-    #print "Title of listing", item['title']
-    for mytags in item['tags']:
-        #make lowercase and add to keywords list
-        keywords.append(mytags.lower())
-#change unicode to ASCII. also .encode("ascii", "ignore") is to force removal of
-#of the BOM unicode stuff.
-wordlist = [str(unicodes.encode("ascii", "ignore")) for unicodes in keywords]
 
-print keywords
-#print json.dumps(decoded['results'], sort_keys=True, indent=4, separators=(',', ': '))
+def gettrendingresponse():
+    r = requests.get('https://openapi.etsy.com/v2/listings/trending?api_key=uyvwtl04yi98duy546afittr&limit=50')
+    #print "Response Code:", r.status_code
+    #decoded = json.loads(r.text)
+    keywords = json.loads(r.text)
+    for item in keywords['results']:
+        #print "Title of listing", item['title']
+        for mytags in item['tags']:
+            #make lowercase and add to keywords list
+            iterkeywords.append(mytags.lower())
+    killunicode(iterkeywords)
 
-def makeDict (wordlist):
-    wordfreq = [wordlist.count(p) for p in wordlist]
-    mydict = dict(zip(wordlist,wordfreq))
-    #sorteddict = sorted(mydict.keys())
+def killunicode(iw):
+
+    #change unicode to ASCII. also .encode("ascii", "ignore") is to force removal of
+    #of the BOM unicode stuff.
+    wordlist = [str(unicodes.encode("ascii", "ignore")) for unicodes in iw]
+    printdict(wordlist)
+
+    #print json.dumps(decoded['results'], sort_keys=True, indent=4, separators=(',', ': '))
+
+def printdict(wl):
+    #count phrase freq
+    wordfreq = [wl.count(p) for p in wl]
+    #make the finished dict to print from
+    mydict = dict(zip(wl,wordfreq))
 
     for key, value in sorted(mydict.iteritems(), key=lambda (k,v): (v,k), reverse=True):
         if value == 1:
             pass
         else:
-            print "Keyword", key, "Freq", value
+            print key, "Freq", value
 
-        #list.append("Count:", value, "Keyword:", key)
-    #return list
+def main():
+    gettrendingresponse()
 
-makeDict(wordlist)
+main()
